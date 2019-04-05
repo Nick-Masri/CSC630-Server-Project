321a0a8bf6cf94a2c51f48852e24c5e582948355
naluconcepcion <naluconcepcion@gmail.com>
2019-04-05 12:24:14 -0400

    paren fix

--- server.js
+++ server.js
@@ -153,8 +153,7 @@ app.post("/user/update", function(req, res) {
 app.post("/user/delete", function(req, res) {
   knex('users_tb').where("user_id", "=", req.body.userId).del().then(function() {
     res.status(200).send('Succesfully Deleted Entry in Users Table');
-    }
-  })
+    })
 };
 
 // Returns a JSON list of a single userIdRef
